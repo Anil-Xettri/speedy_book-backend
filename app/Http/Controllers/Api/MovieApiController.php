5dc86eb80e4e40e764d4e346a7780438772abba3
Anil-Xettri <baniyaanil1203@gmail.com>
2023-04-09 22:49:31 +0545

    fix api error

--- app/Http/Controllers/Api/MovieApiController.php
+++ app/Http/Controllers/Api/MovieApiController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Booking;
+use App\Models\BookingSeat;
 use App\Models\Movie;
+use App\Models\Seat;
 use App\Models\ShowTime;
 use App\Models\Theater;
 use App\Models\Vendor;
@@ -86,11 +88,23 @@ class MovieApiController extends BaseApiController
             $today = new Carbon(Carbon::now('Asia/Kathmandu')->toDateString());
             $tomorrow = new Carbon(Carbon::tomorrow('Asia/Kathmandu')->toDateString());
             $showDetails = [];
+            $todayShows = [];
+            $tomorrowShows = [];
 
             foreach ($movie->showTimes as $showTime) {
                 foreach (json_decode($showTime->show_details, true) as $showDetail) {
-                    if (new Carbon($showDetail['show_date']) == $today || new Carbon($showDetail['show_date']) == $tomorrow) {
-                        $showDetails[] = [
+                    if (new Carbon($showDetail['show_date']) == $today) {
+                        $todayShows[] = [
+                            'show_time_id' => $showTime->id,
+                            'date' => $showDetail['show_date'],
+                            'time' => $showDetail['show_time'],
+                            'price' => $showDetail['ticket_price'],
+                        ];
+                    }
+
+                    if (new Carbon($showDetail['show_date']) == $tomorrow) {
+                        $tomorrowShows[] = [
+                            'show_time_id' => $showTime->id,
                             'date' => $showDetail['show_date'],
                             'time' => $showDetail['show_time'],
                             'price' => $showDetail['ticket_price'],
@@ -100,7 +114,7 @@ class MovieApiController extends BaseApiController
             }
             return response()->json([
                 'success' => true,
-                'data' => ['movie' => $movie, 'show_time' => $showDetails]
+                'data' => ['movie' => $movie, '' . $today . '' => $todayShows, '' . $tomorrow . '' => $tomorrowShows]
             ]);
 
         } catch (\Exception $e) {
@@ -113,6 +127,9 @@ class MovieApiController extends BaseApiController
         try {
             $validator = Validator::make($request->all(), [
                 'show_id' => 'required',
+//                'show_date' => 'required',
+//                'show_time' => 'required',
+                'theater_id' => 'required',
             ]);
             if ($validator->fails()) {
                 $response['success'] = false;
@@ -121,6 +138,7 @@ class MovieApiController extends BaseApiController
             }
 
             $showTime = ShowTime::where('id', $request->show_id)->first();
+            $theater = Theater::where('id', $request->theater_id)->first();
             if (!$showTime) {
                 return response()->json([
                     'success' => false,
@@ -129,28 +147,64 @@ class MovieApiController extends BaseApiController
             }
 
             $seatDetails = [];
+            $bookings = [];
 
             foreach (json_decode($showTime->show_details, true) ?? [] as $showDetails) {
                 $showDate = $showDetails['show_date'];
                 $showTime = $showDetails['show_time'];
                 $showDateTime = $showDate . ' ' . $showTime;
-                $bookings = Booking::where('show_time', $showDateTime)->get();
-                foreach ($bookings as $booking) {
-                    foreach ($booking->seats as $seat) {
-                        $seatDetails[] = [
-                            'booking_id' => $seat->pivot->booking_id,
-                            'seat_id' => $seat->pivot->seat_id,
-                            'seat_name' => $seat->seat_name,
-                            'status' => $seat->pivot->status,
-                        ];
-                    }
+//                $bookingsDetails = Booking::where('show_time', $showDateTime)->get();
+//                foreach ($bookingsDetails ?? [] as $booking) {
+//                    foreach ($booking->seats ?? [] as $seat) {
+//                        $seatDetails[] = [
+//                            'booking_id' => $seat->pivot->booking_id,
+//                            'seat_id' => $seat->pivot->seat_id,
+//                            'seat_name' => $seat->seat_name,
+//                            'status' => $seat->pivot->status,
+//                        ];
+//                    }
+//                }
 
-                }
+                $bookings[] = Booking::where('show_time', $showDateTime)->pluck('id')->toArray();
             }
+
+            $emptySeats = Seat::where(['theater_id' => $request->theater_id, 'seat_name' => ""])->get();
+            foreach ($emptySeats ?? [] as $emptySeat) {
+                $seatDetails[] = [
+                    'booking_id' => null,
+                    'seat_id' => $emptySeat->id,
+                    'seat_name' => "",
+                    'status' => "Unavailable",
+                ];
+            }
+
+            $seatData = BookingSeat::whereIn('booking_id', $bookings[0])->pluck('seat_id')->toArray();
+            $bookingSeats = $seatData;
+            $AvailSeats = Seat::where('theater_id', $request->theater_id)->where('seat_name', '!=', "")->whereNotIn('id', $bookingSeats)->get();
+            foreach ($AvailSeats ?? [] as $availSeat) {
+                $seatDetails[] = [
+                    'booking_id' => null,
+                    'seat_id' => $availSeat->id,
+                    'seat_name' => $availSeat->seat_name,
+                    'status' => "Available",
+                ];
+            }
+
+            $b = BookingSeat::whereIn('booking_id', $bookings[0])->get();
+            foreach ($b ?? [] as $f) {
+                $seatDetails[] = [
+                    'booking_id' => $f->id,
+                    'seat_id' => $f->seat_id,
+                    'seat_name' => $f->seat->seat_name,
+                    'status' => $f->status,
+                ];
+            }
+
+            $array = collect($seatDetails)->sortBy('seat_id');
 
             return response()->json([
                 'success' => true,
-                'data' => $seatDetails
+                'data' => $array
             ]);
 
         } catch (\Exception $e) {
